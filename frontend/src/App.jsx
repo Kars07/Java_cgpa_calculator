ff60ab953481ca4dcb73b6b44e5204a1dd0ac51b
Omilabu Elizabeth Wuraola <omilabuw@gmail.com>
2025-11-21 01:32:43 +0100

    Setup the frontend environment for the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className='text-6xl text-amber-500 text-center py-50 '>Hi Welcome to Raphael's CGPA Calculator</h1>
+    </div>
+  )
+}
+
+export default App
